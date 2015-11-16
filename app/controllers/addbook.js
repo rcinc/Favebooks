c0a3e4f3b076004fc198eecc655dd2ca3a3f8444
Robert Cardona <rfcardona@gmail.com>
2015-11-16 12:43:04 -0500

    First commit

---
+++ app/controllers/addbook.js
@@ -0,0 +1,12 @@
+var myBooks = Alloy.Collections.books;
+
+function addBook(){
+	var book = Alloy.createModel('books', {
+		title : $.titleInput.value,
+		author : $.authorInput.value
+	});
+	myBooks.add(book);
+	book.save();
+	// Closes window
+	$.addbook.close();
+}
---
+++ app/controllers/bookdetails.js
@@ -0,0 +1,4 @@
+var args = arguments[0] || {};
+$.titleLabel.text = args.title || 'Default Title';
+$.authorLabel.text = args.author || 'Default author';
+
